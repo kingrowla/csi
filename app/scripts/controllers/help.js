9e5f490bebf9f75dee5242ea39fd42197d1a547f
kingrowla <joseph.terrell@lazarussoftware.com>
2015-06-19 00:34:18 -0500

    latest commit

--- app/scripts/controllers/help.js
+++ app/scripts/controllers/help.js
@@ -13,31 +13,45 @@ angular.module('csiPublicWebsite2015App')
         }, 200);
         $('.rslidesContainer').hide();
 
-    }).config(function ($stateProvider, $urlRouterProvider) {
+    })
+    .controller('FaqCtrl', function ($scope) {
+        $("html, body").animate({
+            scrollTop: 100
+        }, 200);
+       
+
+    })
+    .config(function ($stateProvider, $urlRouterProvider) {
         var partialViewURL = 'views/help-partial-views/';
         $stateProvider
             .state('help.faq1', {
                 url: '/faq1',
                 templateUrl: partialViewURL + 'faq1.html',
+                controller: 'FaqCtrl'
             })
             .state('help.faq2', {
                 url: '/faq2',
-                templateUrl: partialViewURL + 'faq2.html'
+                templateUrl: partialViewURL + 'faq2.html',
+                controller: 'FaqCtrl'
             })
             .state('help.faq3', {
                 url: '/faq3',
-                templateUrl: partialViewURL + 'faq3.html'
+                templateUrl: partialViewURL + 'faq3.html',
+                controller: 'FaqCtrl'
             })
             .state('help.faq4', {
                 url: '/faq4',
-                templateUrl: partialViewURL + 'faq4.html'
+                templateUrl: partialViewURL + 'faq4.html',
+                controller: 'FaqCtrl'
             })
             .state('help.faq5', {
                 url: '/faq5',
-                templateUrl: partialViewURL + 'faq5.html'
+                templateUrl: partialViewURL + 'faq5.html',
+                controller: 'FaqCtrl'
             })
             .state('help.faq6', {
                 url: '/faq6',
-                templateUrl: partialViewURL + 'faq6.html'
+                templateUrl: partialViewURL + 'faq6.html',
+                controller: 'FaqCtrl'
             });
     });
